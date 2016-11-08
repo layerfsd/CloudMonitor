802e7b853313acc11da9b9644a9648362953e532
albertofwb <albertwb951@gmail.com>
2016-11-08 10:42:17 +0800

    import Monitor Global System IO events to Project

---
+++ MonitroService/MonitroService.cpp
@@ -0,0 +1,30 @@
+// MonitroService.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <signal.h>
+
+
+typedef void(*SignalHandlerPointer)(int);
+
+void SignalHandler(int signal)
+{
+	printf("\nExciting...\n");
+	SetHookOff();
+	exit(signal);
+}
+
+int main()
+{
+	SignalHandlerPointer previousHandler;
+	previousHandler = signal(SIGINT, SignalHandler);
+
+	SetHookOn();
+	printf("HELO\n");
+	while (1)
+	{
+		Sleep(50);
+	}
+    return 0;
+}
+
