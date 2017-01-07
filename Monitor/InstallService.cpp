1c8be50a3c957c7d516503f07ab7a61afabbba0f
albertofwb <albertofwb@gmail.com>
2017-01-07 22:41:01 +0800

    调整创建服务的格式

--- Monitor/InstallService.cpp
+++ Monitor/InstallService.cpp
@@ -5,6 +5,23 @@
 // if not SERVICE_NAME installed
 // install the service.
 
+
+
+bool SysRun(const char* cmd)
+{
+	FILE* execfd = NULL;
+
+	printf("cmd: [%s]\n", cmd);
+
+	execfd = _popen(cmd, "r");
+
+	if (NULL == execfd)
+	{
+		return false;
+	}
+
+	return (0 == _pclose(execfd));
+}
 void InstallService()
 {
 	char cmd[MAX_PATH] = { 0 };
@@ -14,6 +31,7 @@ void InstallService()
 	GetCurrentDirectoryA(sizeof(workPath), workPath);
 	snprintf(ServicePath, sizeof(ServicePath), "%s\\%s", workPath, SERVICE_APP);
 
-	snprintf(cmd, sizeof(cmd), "sc create %s binpath=\"%s\" start=delayed-auto", SERVICE_NAME,  ServicePath);
-	system(cmd);
+
+	snprintf(cmd, sizeof(cmd), "sc create %s binpath= \"%s\" start= delayed-auto", SERVICE_NAME, ServicePath);
+	SysRun(cmd);
 }
