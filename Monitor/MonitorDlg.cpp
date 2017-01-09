d16dad71d782ae58de56634f688c52e0fc7030e8
albertofwb <albertofwb@gmail.com>
2017-01-10 00:16:00 +0800

    取消登陆前网络状态检测

--- Monitor/MonitorDlg.cpp
+++ Monitor/MonitorDlg.cpp
@@ -18,51 +18,11 @@
 #define new DEBUG_NEW
 #endif
 
-#include <WtsApi32.h>
 
-// 以当前用户身份启动一个进程
-bool StartInteractiveProcess(LPSTR cmd, LPSTR cmdDir)
-{
-	STARTUPINFOA si;
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	si.lpDesktop = "winsta0\\default";  // Use the default desktop for GUIs
-	PROCESS_INFORMATION pi;
-	ZeroMemory(&pi, sizeof(pi));
-	HANDLE token;
-	DWORD sessionId = ::WTSGetActiveConsoleSessionId();
-	if (sessionId == 0xffffffff)  // Noone is logged-in
-		return false;
-	// This only works if the current user is the system-account (we are probably a Windows-Service)
-	HANDLE dummy;
-	if (::WTSQueryUserToken(sessionId, &dummy)) {
-		if (!::DuplicateTokenEx(dummy, TOKEN_ALL_ACCESS, NULL, SecurityDelegation, TokenPrimary, &token)) {
-			::CloseHandle(dummy);
-			return false;
-		}
-		::CloseHandle(dummy);
-		// Create process for user with desktop
-		if (!::CreateProcessAsUserA(token, NULL, cmd, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, cmdDir, &si, &pi)) {  // The "new console" is necessary. Otherwise the process can hang our main process
-			::CloseHandle(token);
-			return false;
-		}
-		::CloseHandle(token);
-	}
-	// Create process for current user
-	else if (!::CreateProcessA(NULL, cmd, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, cmdDir, &si, &pi))  // The "new console" is necessary. Otherwise the process can hang our main process
-		return false;
-	// The following commented lines can be used to wait for the process to exit and terminate it
-	//::WaitForSingleObject(pi.hProcess, INFINITE);
-	//::TerminateProcess(pi.hProcess, 0);
-	::CloseHandle(pi.hProcess);
-	::CloseHandle(pi.hThread);
-	return true;
-}
 // CMonitorDlg 对话框
 
 
 void InstallService();
-bool StartInteractiveProcess(const char* cmd, LPCTSTR cmdDir);
 
 CMonitorDlg::CMonitorDlg(CWnd* pParent /*=NULL*/)
 	: CDialogEx(IDD_MONITOR_DIALOG, pParent)
@@ -297,6 +257,7 @@ void CMonitorDlg::OnBnClickedOk()
 		AfxMessageBox(inform);
 		return;
 	}
+	char cWinDir[MAX_PATH];
 	char cmd[MAX_PATH];
 	const char *cname;
 	const char *cpass;
@@ -317,19 +278,14 @@ void CMonitorDlg::OnBnClickedOk()
 		return;
 	}
 
-	if (0 != IsCnt2Internet())
-	{
-		inform = "您的网络状况异常";
-		//SetDlgItemText(IDC_STATUS, inform);
-		SetDlgItemText(IDC_STATUS, NULL);
-		AfxMessageBox(inform);
-		return;
-	}
-
-
 	memset(cmd, 0, MAX_PATH);
+	GetCurrentDirectoryA(MAX_PATH, cWinDir);
 
-	sprintf_s(cmd, MAX_PATH, "%s %s %s", DAEMON_APP, cname, cpass);
+	char* sAppPath = cWinDir;
+	strcat_s(cWinDir, sizeof(cWinDir), "\\CloudMonitor.exe");
+
+	sprintf_s(cmd, MAX_PATH, "%s %s %s", sAppPath, cname, cpass);
+
 
 	STARTUPINFOA   StartupInfo;//创建进程所需的信息结构变量    
 	PROCESS_INFORMATION pi;
@@ -341,16 +297,29 @@ void CMonitorDlg::OnBnClickedOk()
 
 
 	// 在后台开启 CloudMonitor.exe 进程
-	if (StartInteractiveProcess(cmd, NULL))
+	if (CreateProcessA(NULL,
+		cmd,
+		NULL, 
+		NULL, 
+		FALSE, 
+		//0,
+		CREATE_NO_WINDOW,
+		NULL,
+		NULL, 
+		&StartupInfo,
+		&pi))
 	{
 
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
 		inform = "正在登录 ...";
 		SetDlgItemText(IDC_STATUS, inform);
 
 	}
 	else
 	{
-		inform = "登陆失败";
+		inform = sAppPath;
+		inform += "登陆失败";
 		SetDlgItemText(IDC_STATUS, NULL);
 		AfxMessageBox(inform);
 		CDialogEx::OnOK();
