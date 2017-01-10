018b14b3bb7e0454e9de0e89b572eda69ac33dba
albertofwb <albertofwb@gmail.com>
2017-01-10 14:48:08 +0800

    修改登录程序，如果检测到服务已安装，则不再尝试去安装服务

--- Monitor/InstallService.cpp
+++ Monitor/InstallService.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <winsvc.h>
 
 #define SERVICE_NAME  "CloudMonitorService"
 #define SERVICE_APP	  "Service.exe"
@@ -22,6 +23,41 @@ bool SysRun(const char* cmd)
 
 	return (0 == _pclose(execfd));
 }
+
+
+int GetServiceStatus(const char* name)
+{
+	SC_HANDLE theService, scm;
+	SERVICE_STATUS_PROCESS ssStatus;
+	DWORD dwBytesNeeded;
+
+
+	scm = OpenSCManager(nullptr, nullptr, SC_MANAGER_ENUMERATE_SERVICE);
+	if (!scm) {
+		return 0;
+	}
+
+	theService = OpenServiceA(scm, name, SERVICE_QUERY_STATUS);
+	if (!theService) {
+		CloseServiceHandle(scm);
+		return 0;
+	}
+
+	auto result = QueryServiceStatusEx(theService, SC_STATUS_PROCESS_INFO,
+		reinterpret_cast<LPBYTE>(&ssStatus), sizeof(SERVICE_STATUS_PROCESS),
+		&dwBytesNeeded);
+
+	CloseServiceHandle(theService);
+	CloseServiceHandle(scm);
+
+	if (result == 0) {
+		return 0;
+	}
+
+	return ssStatus.dwCurrentState;
+}
+
+
 void InstallService()
 {
 	char cmd[MAX_PATH] = { 0 };
@@ -29,9 +65,12 @@ void InstallService()
 	char ServicePath[MAX_PATH] = { 0 };
 
 	GetCurrentDirectoryA(sizeof(workPath), workPath);
-	snprintf(ServicePath, sizeof(ServicePath), "%s\\%s", workPath, SERVICE_APP);
 
+	if (GetServiceStatus(SERVICE_NAME) == 0)
+	{
+		snprintf(ServicePath, sizeof(ServicePath), "%s\\%s", workPath, SERVICE_APP);
+		snprintf(cmd, sizeof(cmd), "sc create %s binpath= \"%s\" start= delayed-auto", SERVICE_NAME, ServicePath);
+		SysRun(cmd);
+	}
 
-	snprintf(cmd, sizeof(cmd), "sc create %s binpath= \"%s\" start= delayed-auto", SERVICE_NAME, ServicePath);
-	SysRun(cmd);
 }
