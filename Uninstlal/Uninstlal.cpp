49bb95fe7434466259ed9f36eb97c41ab7a43ea2
albertofwb <albertofwb@gmail.com>
2017-01-20 10:08:48 +0800

    测试发现在(C:\Programs Files)目录下无法启动监控程序

---
+++ Uninstlal/Uninstlal.cpp
@@ -0,0 +1,40 @@
+// Uninstlal.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#define ArraySize(ptr)		(sizeof(ptr) / sizeof(ptr[0]))
+
+bool SysRun(const char* cmd)
+{
+	FILE* execfd = NULL;
+
+	printf("cmd: [%s]\n", cmd);
+
+	execfd = _popen(cmd, "r");
+
+	if (NULL == execfd)
+	{
+		return false;
+	}
+
+	return (0 == _pclose(execfd));
+}
+
+int main()
+{
+	const char *cmds[]{
+		"sc stop CloudMonitorService",
+		"sc delete CloudMonitorService",
+		"taskkill /f /im CloudMonitor.exe",
+		"taskkill /f /im MonitorService.exe",
+		"taskkill /f /im MonitorService-64.exe",
+	};
+
+	for (int i = 0; i < ArraySize(cmds); i++)
+	{
+		SysRun(cmds[i]);
+	}
+    return 0;
+}
+
