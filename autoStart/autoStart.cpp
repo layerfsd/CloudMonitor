25862ce635c90f3bd2179e2203f345f99eba474c
albertofwb <albertofwb@gmail.com>
2017-02-16 16:08:54 +0800

    新特性:安装程序启动注册客户端后自动关闭安装窗口

---
+++ autoStart/autoStart.cpp
@@ -0,0 +1,130 @@
+// autoStart.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <windows.h>
+#include <tlhelp32.h>
+
+#define INSTALL_WINDOW_TITLE "安全监管客户端"
+
+
+void SetWorkPath()
+{
+	char strModule[MAX_PATH];
+	GetModuleFileName(NULL, strModule, MAX_PATH); //得到当前模块路径
+	strcat(strModule, "\\..\\");     //设置为当前工作路径为当时的上一级
+	SetCurrentDirectory(strModule);
+}
+
+
+
+bool MyCreateProcess(LPCSTR appName, LPSTR appArgs)
+{
+	STARTUPINFOA   StartupInfo;		//创建进程所需的信息结构变量    
+	PROCESS_INFORMATION pi;
+	char output[MAXBYTE];
+
+	if (NULL == appName)
+	{
+		return false;
+	}
+
+	ZeroMemory(&pi, sizeof(pi));
+	ZeroMemory(&StartupInfo, sizeof(StartupInfo));
+
+	StartupInfo.cb = sizeof(StartupInfo);
+
+	snprintf(output, sizeof(output), "%s %s", appName, appArgs);
+
+	if (CreateProcessA(NULL,
+		output,
+		NULL,
+		NULL,
+		FALSE,
+		//0,
+		CREATE_NO_WINDOW,
+		NULL,
+		NULL,
+		&StartupInfo,
+		&pi))
+	{
+
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
+	else
+	{
+		snprintf(output, MAXBYTE, "[ERROR] CreateProcess: %s", appName);
+		return false;
+	}
+
+	return true;
+}
+
+
+BOOL FindProcessPid(LPCSTR ProcessName, DWORD& dwPid)
+{
+	HANDLE hProcessSnap;
+	PROCESSENTRY32 pe32;
+
+	// Take a snapshot of all processes in the system.
+	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		return(FALSE);
+	}
+
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		CloseHandle(hProcessSnap);          // clean the snapshot object
+		return(FALSE);
+	}
+
+	BOOL	bRet = FALSE;
+	do
+	{
+		// 忽略大小写
+		if (!_stricmp(ProcessName, pe32.szExeFile))
+		{
+			dwPid = pe32.th32ProcessID;
+			bRet = TRUE;
+			break;
+		}
+
+	} while (Process32Next(hProcessSnap, &pe32));
+
+	CloseHandle(hProcessSnap);
+	return bRet;
+}
+
+// 关闭安装页面
+int killInstallWindow()
+{
+	HWND hnw = FindWindow(NULL, INSTALL_WINDOW_TITLE);
+	if (NULL == hnw) {
+		return -1;
+	}
+	DWORD dnp = 0;
+	GetWindowThreadProcessId(hnw, &dnp);
+	if (0 == dnp) {
+		return -1;
+	}
+	HANDLE hnh = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dnp);
+	if (NULL == hnh) {
+		return -1;
+	}
+	TerminateProcess(hnh, 0);
+	CloseHandle(hnw);
+}
+
+
+int main()
+{
+	killInstallWindow();
+	SetWorkPath();
+	MyCreateProcess("Monitor.exe", NULL);
+    return 0;
+}
+
