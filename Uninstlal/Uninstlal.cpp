312c6eaeea480ede7e4319c67577d7bc3700fe68
albertofwb <albertofwb@gmail.com>
2017-02-16 16:20:32 +0800

    隐藏安装和卸载时的环境监测窗口

--- Uninstlal/Uninstlal.cpp
+++ Uninstlal/Uninstlal.cpp
@@ -1,7 +1,5 @@
-﻿// Uninstlal.cpp : �������̨Ӧ�ó������ڵ㡣
-//
-
-#include "stdafx.h"
+﻿#include "stdafx.h"
+#include <windows.h>
 
 #define ArraySize(ptr)		(sizeof(ptr) / sizeof(ptr[0]))
 
@@ -21,7 +19,56 @@ bool SysRun(const char* cmd)
 	return (0 == _pclose(execfd));
 }
 
-int main()
+
+bool MyCreateProcess(LPCSTR appName, LPSTR appArgs=NULL)
+{
+	STARTUPINFOA   StartupInfo;		//创建进程所需的信息结构变量    
+	PROCESS_INFORMATION pi;
+	char output[MAXBYTE];
+
+	if (NULL == appName)
+	{
+		return false;
+	}
+
+	ZeroMemory(&pi, sizeof(pi));
+	ZeroMemory(&StartupInfo, sizeof(StartupInfo));
+
+	StartupInfo.cb = sizeof(StartupInfo);
+
+	snprintf(output, sizeof(output), "%s", appName);
+
+	if (CreateProcessA(NULL,
+		output,
+		NULL,
+		NULL,
+		FALSE,
+		//0,
+		CREATE_NO_WINDOW,
+		NULL,
+		NULL,
+		&StartupInfo,
+		&pi))
+	{
+
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
+	else
+	{
+		snprintf(output, MAXBYTE, "[ERROR] CreateProcess: %s", appName);
+		return false;
+	}
+
+	return true;
+}
+
+
+int __stdcall WinMain(HINSTANCE hInstance,      // handle to current instance
+	HINSTANCE hPrevInstance,  // handle to previous instance
+	LPSTR lpCmdLine,          // command line
+	int nCmdShow              // show state
+	)
 {
 	const char *cmds[]{
 		"sc stop CloudMonitorService",
@@ -33,7 +80,7 @@ int main()
 
 	for (int i = 0; i < ArraySize(cmds); i++)
 	{
-		SysRun(cmds[i]);
+		MyCreateProcess(cmds[i]);
 	}
     return 0;
 }
