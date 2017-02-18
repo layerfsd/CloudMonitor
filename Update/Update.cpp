3f1ce1cde5993d7de041a785f6d3ff66ceb3ef48
albertofwb <albertofwb@gmail.com>
2017-02-18 18:57:51 +0800

    自更新程序中加入更多日志输出

--- Update/Update.cpp
+++ Update/Update.cpp
@@ -82,6 +82,8 @@ int main(int argc, char *argv[])
 		printf("GetLatestVersion() FAILED\n");
 		return 1;
 	}
+	WriteToLog("GetLatestVersion Ok");
+
 
 	if (!ver.WhetherUpdate())
 	{
@@ -89,31 +91,38 @@ int main(int argc, char *argv[])
 		WriteToLog("Already Latest Version.\n");
 		return 0;
 	}
+	WriteToLog("Begin Fetch HashList");
+
 
 	// 获取最新版本的文件哈希列表
 	if (!ver.RequestHashList())
 	{
 		printf("Request Latest HashList failed.\n");
-		WriteToLog("Request Latest HashList failed.\n");
+		WriteToLog("Request Latest HashList failed");
 		return 1;
 	}
+	WriteToLog("Request Latest HashList Ok");
+
 
 	// 根据文件哈希不同，获取所有‘更新’的文件
 	if (!ver.DownloadLatestFiles(TMPDOWN_DIR))
 	{
 		printf("Download Latest Files failed.\n");
-		WriteToLog("Download Latest Files failed.\n");
+		WriteToLog("Download Latest Files failed");
 		return 1;
 	}
+	WriteToLog("Download Latest Files Ok");
+
 
 	// 用临时目录中的文件替换安装根目录的文件
 	if (!ver.ReplaceFiles(TMPDOWN_DIR))
 	{ 
 		printf("Replace Files failed.\n");
-		WriteToLog("Replace Files failed.\n");
+		WriteToLog("Replace Files failed");
 		ver.RollBack();
 		return 1;
 	}
+	WriteToLog("Replace Files Ok");
 	
 	return 0;
 }
